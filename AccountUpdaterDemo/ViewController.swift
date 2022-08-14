2dc4474d3d4cab2149f2b68756cd33365d0a2daf
Tarik AIT M'BAREK <taitmbarek@gmail.com>
2022-08-14 22:24:10 +0100

    Initial Commit

---
+++ AccountUpdaterDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountUpdaterDemo
+//
+//  Created by TARIK AIT MBAREK on 14/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
