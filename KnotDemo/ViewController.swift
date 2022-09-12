abfc70697051cde39249785be3cc8054285fd3fc
Tarik AIT M'BAREK <taitmbarek@gmail.com>
2022-09-12 13:24:16 +0100

    update to KnotAPI 0.1.15 and change icon and bundle id

---
+++ KnotDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AccountUpdaterDemo
+//
+//  Created by TARIK AIT MBAREK on 14/8/2022.
+//
+
+import UIKit
+import KnotAPI
+
+class ViewController: UIViewController {
+    var session: CardOnFileSwitcherSession!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func openAccountUpdater() {
+        let session = CardOnFileSwitcherSession(sessionId: "e64b25c2-d307-41bf-abc1-d3dd1803e2f2")
+        session.openOnCardFileSwitcher(merchants: [])
+    }
+}
+
--- Podfile
+++ Podfile
@@ -1,10 +1,10 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '10.0'
 
-target 'AccountUpdaterDemo' do
+target 'KnotDemo' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-  pod 'KnotAPI', '~> 0.1.14'
+  pod 'KnotAPI', '~> 0.1.15'
 
   # Pods for AccountUpdaterDemo
 
