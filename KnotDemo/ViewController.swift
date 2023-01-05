4850bdea028c37bbcd973371317956f4879c036d
Tarik AIT M'BAREK <taitmbarek@gmail.com>
2023-01-05 12:40:29 +0100

    set IPHONEOS_DEPLOYMENT_TARGET to 11

--- KnotDemo/ViewController.swift
+++ KnotDemo/ViewController.swift
@@ -32,7 +32,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        overrideUserInterfaceStyle = .light
+        if #available(iOS 13.0, *) {
+            overrideUserInterfaceStyle = .light
+        } else {
+            // Fallback on earlier versions
+        }
         // Do any additional setup after loading the view.
     }
 
--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ platform :ios, '11.0'
 target 'KnotDemo' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
+  pod 'KnotAPI', '0.1.44'
   # Pods for KnotDemo
 
 end
